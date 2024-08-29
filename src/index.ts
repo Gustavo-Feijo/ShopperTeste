7f2c8627f9fce11e4c14b8d2988f9a66f1a3a4ac
Gustavo Feijó <gustavofeijo.dev@gmail.com>
2024-08-29 14:41:07 -0300

    ADD: Created basic patch and get routes.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 import { config } from "dotenv";
+import { z } from "zod";
+import { PatchSchema, UploadSchema } from "./validationSchemas";
+import prisma from "./db";
+import { MeasureType } from "@prisma/client";
 
 // Load the .env file and look for errors.
 const keyLoad = config();
@@ -15,15 +19,138 @@ if (!apiKey) {
 }
 
 console.log("Gemini API Key found.");
+
 // Instanciate the express application.
 const app = express();
 
 // Parse the requests to json.
 app.use(express.json());
 
-app.post("/upload", (req, res) => {
-  console.log(req.body);
+// TODO
+app.post("/upload", async (req, res) => {
+  try {
+    await UploadSchema.parseAsync(req.body);
+  } catch (err) {
+    console.error(err);
+  }
 });
+
+// Patch route for confirming and updating a stored measure.
+app.patch("/confirm", async (req, res) => {
+  try {
+    // Parse the request body with the validation schema.
+    const validatedData = await PatchSchema.parseAsync(req.body);
+
+    // Get the measure from the database.
+    const measure = await prisma.measure.findUnique({
+      where: { id: validatedData.measure_uuid },
+    });
+
+    // Handle the case where no measure was found.
+    if (!measure) {
+      return res.status(404).json({
+        error_code: "MEASURE_NOT_FOUND",
+        error_description: "The specified measure was not found.",
+      });
+    }
+
+    // Handle the case of a already confirmated measure.
+    if (measure.confirmed) {
+      return res.status(409).json({
+        error_code: "validatedData_DUPLICATE",
+        error_description: "The specified measure was already confirmed.",
+      });
+    }
+
+    // Update the measure on the database.
+    await prisma.measure.update({
+      data: { confirmed: true, value: validatedData.confirmed_value },
+      where: { id: validatedData.measure_uuid },
+    });
+
+    // Return a sucess response.
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (err) {
+    // Verify if an error is a input error.
+    if (err instanceof z.ZodError) {
+      return res.status(400).json({
+        error_code: "INVALID_DATA",
+        error_description: err.message,
+      });
+    }
+
+    // Return a response for unhandled errors.
+    console.error(`Unhandled Error:${err}`);
+    return res.status(500).json({
+      error_code: "INTERNAL_SERVER_ERROR",
+      error_description: "An unexpected error occurred.",
+    });
+  }
+});
+
+// Get Route for getting the list of measures for a given user.
+app.get("/:customerCode/list", async (req, res) => {
+  try {
+    // Get the customer code and measureType from the request.
+    const customerCode = req.params.customerCode;
+    let measureType = req.query.measure_type;
+
+    // Verify the measure type
+    if (measureType) {
+      // Handle invalid measure types.
+      if (typeof measureType !== "string") {
+        return res.status(400).json({
+          error_code: "INVALID_MEASURE_TYPE",
+          error_description:
+            "Couldn't get a valid measure type from the query.",
+        });
+      }
+
+      // Parse the measure type to assure they are correct.
+      measureType = await z
+        .enum(["WATER", "GAS"], { message: "Invalid measure type." })
+        .parseAsync(measureType.toUpperCase());
+    }
+
+    // Get the data from the database.
+    const measures = await prisma.measure.findMany({
+      where: {
+        customerCode: customerCode,
+        ...(measureType && { type: measureType as MeasureType }),
+    },select:{"id":true}
+    });
+
+    // If no measure is found, return a 404 response.
+    if (measures.length === 0) {
+      return res.status(404).json({
+        error_code: "MISSING_MEASURES",
+        error_description: "No measure was found.",
+      });
+    }
+
+    // Return a sucess response.
+    return res
+      .status(200)
+      .json({ customer_code: customerCode, measures: measures });
+  } catch (err) {
+    // Handle a ZodError in the case the enum was not parsed correctly.
+    if (err instanceof z.ZodError) {
+      return res
+        .status(400)
+        .json({ error_code: "INVALID_TYPE", error_description: err.message });
+    }
+
+    // Return a response for unhandled errors.
+    console.error(`Unhandled Error:${err}`);
+    return res.status(500).json({
+      error_code: "INTERNAL_SERVER_ERROR",
+      error_description: "An unexpected error occurred.",
+    });
+  }
+});
+
 // Get the port and start the HTTP server.
 const port = process.env.PORT || 3000;
 app.listen(port);
